f87d226511946d8caffbdb1c4ce7224fedab4512
mightyMarabu <sebastian.n.schmidt@web.de>
2018-05-21 23:16:16 +0200

    init

---
+++ app/go.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, Response, jsonify
+from db_conn import insert_into_db, resetMap
+import json
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/save/")
+def submit():
+    sub = {"id":"2"}
+    sub = ["What am", "I doing?"]
+    sub = insert_into_db()
+    return Response(json.dumps(sub), mimetype = "application/json")
+
+@app.route("/save/<lat>/<lng>/<radius>")
+def savePoint(lat, lng, radius):
+    sub = insert_into_db("landbook.insert_data", (lat,lng,radius))
+    return jsonify("data submitted!")
+
+#@app.route("/reset/")
+#def reset():
+#    res = {"id":"99"}
+#    res = ["What am", "I doing?"]
+#    res = resetMap()
+#    return Response(json.dumps(res), mimetype = "application/json")   
+
+@app.route("/reset/")
+def reset():
+    res = {"id":"3"}
+    res = resetMap()
+    return jsonify("map reseted!")
+#    return Response(json.dumps(res), mimetype = "application/json")
+
+if __name__ == "__main__":
+#    app.run(debug = True)
+    app.run(host='0.0.0.0', port=80)
---
+++ app/static/js/resources/core.js
@@ -0,0 +1,56 @@
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+closer.onclick = function() {
+    container.style.display = 'none';
+    closer.blur();
+    return false;
+};
+var overlayPopup = new ol.Overlay({
+    element: container
+});
+
+var expandedAttribution = new ol.control.Attribution({
+    collapsible: false
+});
+
+var map = new ol.Map({
+    controls: ol.control.defaults({attribution:false}).extend([
+        expandedAttribution
+    ]),
+    target: document.getElementById('map'),
+    renderer: 'canvas',
+    overlays: [overlayPopup],
+    layers: layersList,
+    view: new ol.View({
+         maxZoom: 28, minZoom: 1
+    })
+});
+
+var layerSwitcher = new ol.control.LayerSwitcher({tipLabel: "Layers"});
+map.addControl(layerSwitcher);
+layerSwitcher.hidePanel = function() {};
+layerSwitcher.showPanel();
+
+map.on("singleclick", (e) => {
+    console.log("clicked");
+    var xy = ol.proj.transform(e.coordinate, "EPSG:3857", "EPSG:4326");
+    var radius = parseFloat($("#radius").val());
+    console.log(xy[0], xy[1]);
+    console.log(radius);
+    $.getJSON({
+        url: `/save/${xy[0]}/${xy[1]}/${radius}`,
+        success: data => {
+            console.log(data);
+            location.reload();
+        }
+    }).error(function(jqXHR, textStatus, errorThrown) {
+        console.log("error " + textStatus);
+        console.log("incoming Text " + jqXHR.responseText);
+    });
+});
+
+
+map.getView().fit([2200000, 200000, 6200000, 800000], map.getSize());
+var NO_POPUP = 0
+var ALL_FIELDS = 1
